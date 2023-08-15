963a4c7266f35fda7a8399a84a59988c631a7ae6
Xiaoran <xc2422@columbia.edu>
2023-08-14 17:04:10 -0700

    added in java and analysis api

---
+++ public/js/budgetAnlysis.js
@@ -0,0 +1,72 @@
+const newRevenueAnalysis = async (event) => {
+  event.preventDefault();
+  try {
+    if (newRevenue) {
+      const response = await fetch("/api/revenue", {
+        method: "GET"
+        body: JSON.stringify({ newRevenue }),
+        headers: { "Content-Type": "application/json" },
+      });
+
+      if (response.ok) {
+        document.location.replace("/items");
+      } else {
+        let errorMessage = "";
+
+        switch (response.status) {
+          case 401:
+            errorMessage = "Budget already exists!";
+            break;
+          case 500:
+            errorMessage = "Server error.";
+            break;
+          default:
+            errorMessage = "Unknown error.";
+            break;
+        }
+        errorElement.textContent = errorMessage;
+      }
+    }
+  } catch (err) {
+    console.log(err);
+  }
+};
+
+
+const newExpenseAnalysis = async (event) => {
+    event.preventDefault();
+    try {
+      if (newExpense) {
+        const response = await fetch("/api/expense", {
+          method: "GET"
+          body: JSON.stringify({ newExpense }),
+          headers: { "Content-Type": "application/json" },
+        });
+  
+        if (response.ok) {
+          document.location.replace("/items");
+        } else {
+          let errorMessage = "";
+  
+          switch (response.status) {
+            case 401:
+              errorMessage = "Budget already exists!";
+              break;
+            case 500:
+              errorMessage = "Server error.";
+              break;
+            default:
+              errorMessage = "Unknown error.";
+              break;
+          }
+          errorElement.textContent = errorMessage;
+        }
+      }
+    } catch (err) {
+      console.log(err);
+    }
+  };
+
+document.selectgetElementById("#submit-budget").addEventListener
+("click", function () {document.location.replace("/budgetanalysis")})
+
